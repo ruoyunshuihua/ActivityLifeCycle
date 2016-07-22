ada6313ee71cb0aae1e0dfe42e20579daf7a265a
yfx <25832709782@qq.com>
2016-07-22 14:37:12 +0800

    Activities

---
+++ app/src/main/java/com/ls/activitylifecycle/ActivitySecond.java
@@ -0,0 +1,10 @@
+package com.ls.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class ActivitySecond extends AppCompatActivity {
+
+}
