7a6fce67aac939ef6fe8035d20cdb00cb025c1e1
yfx <25832709782@qq.com>
2016-07-23 16:32:44 +0800

    23日更33Activityty

--- app/src/main/java/com/ls/activitylifecycle/ActivitySecond.java
+++ app/src/main/java/com/ls/activitylifecycle/ActivitySecond.java
@@ -1,10 +1,43 @@
 package com.ls.activitylifecycle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
 public class ActivitySecond extends AppCompatActivity {
+    private TextView mtextView;
+    private Button mSendbackButton;
+    private EditText mNameEditText;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        String inforContent = getIntent().getStringExtra(MainActivity.Book_NAME_OF_CATEGORY_PHYSICAL);
+        //Toast.makeText(ActivitySecond.this, inforContent, Toast.LENGTH_SHORT).show();
+        mtextView = (TextView) findViewById(R.id.second_activity_text_view);
+        mtextView.setText(inforContent);
+        mNameEditText = (EditText) findViewById(R.id.second_activity_edit_text);
+        mSendbackButton = (Button) findViewById(R.id.second_activity_button);
+
+        mSendbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameEditText.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra("back", name);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
 }
--- app/src/main/java/com/ls/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/ls/activitylifecycle/MainActivity.java
@@ -7,28 +7,64 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //private final String TAG= "MainActivity" ;
     private static final String TAG = "MainActivity";
+    public static final String Book_NAME_OF_CATEGORY_PHYSICAL = "bookNameOfCategoryPhysical";
+    private Button mButton;
+    private EditText mEditText;
+    private Button mThirdButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
-        Button button = (Button) findViewById(R.id.activity_main_button);
-        button.setOnClickListener(new View.OnClickListener(){
+        mButton = (Button) findViewById(R.id.activity_main_button);
+        final EditText editText = (EditText) findViewById(R.id.activity_main_edit_text);
+        //final String inforContent =editText.getText().toString();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                //Intent intent = new Intent(MainActivity.this,ActivitySecond.class);
-                //startActivity(intent);
-                Toast.makeText(MainActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ");
+                Intent intent = new Intent(MainActivity.this, ActivitySecond.class);
+                //intent.putExtra("name", "xxxxxxxxxxxxxxx");
+                intent.putExtra(Book_NAME_OF_CATEGORY_PHYSICAL, mEditText.getText().toString());
+                //intent.putExtra("name", "xxxxxxxxxxxxxxx");
+                //intent.putExtra("name",editText.getText().toString());
+                startActivityForResult(intent, 1);
+                //Toast.makeText(MainActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+        mThirdButton = (Button) findViewById(R.id.activity_main_third_button);
+        mThirdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
             }
         });
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //String name = data.getStringExtra("back");
+        String name = "";
+        switch (requestCode) {
+            case 0:
+                name = data.getStringExtra("back");
+                break;
+            case 1:
+                name = data.getStringExtra("backThird");
+        }
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
     }
 
     @Override
